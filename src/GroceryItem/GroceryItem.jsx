a8679ed81d2b8b1017848187a5a2b52594cbf397
Slaven B <slaven.bunijevac@gmail.com>
2022-03-27 22:45:12 +0200

    add styling to icon container

--- src/GroceryItem/GroceryItem.jsx
+++ src/GroceryItem/GroceryItem.jsx
@@ -8,8 +8,8 @@ const GroceryItem = () => {
     <div className={classes.item}>
       <p className={classes.text}>This is my grocery item</p>
       <div className={classes.iconContainer}>
-        <AiOutlineCheck />
-        <FiTrash2 />
+        <AiOutlineCheck className={classes.check} />
+        <FiTrash2 className={classes.trash} />
       </div>
     </div>
   )
