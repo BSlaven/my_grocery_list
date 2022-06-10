fb5e397ad60d7d7447f6448764109835addc9dc8
Slaven B <slaven.bunijevac@gmail.com>
2022-06-10 15:39:20 +0200

    add function to fetch data from local storage

--- src/GroceryLIst/GroceryList.jsx
+++ src/GroceryLIst/GroceryList.jsx
@@ -1,10 +1,22 @@
-
+import { useEffect, useState } from 'react';
 
 import GroceryItem from '../GroceryItem/GroceryItem';
 
 import classes from './GroceryList.module.css';
 
 const GroceryList = () => {
+
+  const [ items, setItems ] = useState([]);
+
+  useEffect(() => {
+    fetchItemsFromStorage();
+  }, []);
+
+  const fetchItemsFromStorage = async () => {
+    const data = await JSON.parse(localStorage.getItems('grocery-items')) || [];
+    setItems(data);
+  }
+  
   return (
     <div className={classes.list}>
       <GroceryItem />
