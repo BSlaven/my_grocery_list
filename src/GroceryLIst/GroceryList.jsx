8b33298ae489566f82738d6ee377894343df3249
Slaven B <slaven.bunijevac@gmail.com>
2022-06-14 11:57:17 +0200

    add clear all button under the list

--- src/GroceryLIst/GroceryList.jsx
+++ src/GroceryLIst/GroceryList.jsx
@@ -101,6 +101,7 @@ const GroceryList = () => {
       </form>
       {error && <p className={classes.errorMessage}>{error}</p>}
       {items && items.map(item => <GroceryItem key={item.id} selectItem={selectItem} deleteItem={deleteItem} {...item} />)}
+      <button className={classes.celarAllBtn}>Clear all</button>
     </div>
   )
 }
